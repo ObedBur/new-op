cfd8d8e2f7ca2e8218f2084f135e7e8b8055fe63
ObedBur <obedburindi@gmail.com>
2026-05-08 12:52:54 +0300

    some update of database

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,14 +1,9 @@
+import "dotenv/config";
 import { defineConfig, env } from "@prisma/config";
-import * as dotenv from "dotenv";
-
-dotenv.config();
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
-  },
-  migrations: {
-    seed: 'ts-node prisma/seed.ts',
+       url: env("DATABASE_URL"), 
   },
 });
